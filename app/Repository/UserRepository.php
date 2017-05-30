5e62ccf9868eb301739f27942ef34cc435814342
spopovic14 <s_popovic14@raf.edu.rs>
2017-05-30 14:44:26 +0200

    Added TABLE_NAME to UserRepository example

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -2,16 +2,17 @@
 
 namespace App\Repository;
 
-use App\Model\User;
 use Core\Base\Repository;
 use Core\Database\Connection;
 
 class UserRepository extends Repository
 {
+	
+    const TABLE_NAME = 'user';
     
     public function getById($id)
     {
-        $statement = $this->getDb()->prepare('SELECT id, name FROM ' . User::TABLE_NAME . ' where id = :id');
+        $statement = $this->getDb()->prepare('SELECT id, name FROM ' . static::TABLE_NAME . ' where id = :id');
 		$statement->setFetchMode(Connection::FETCH_CLASS, 'App\Model\User');
 		$statement->execute([':id' => $id]);
 		$user = $statement->fetch();

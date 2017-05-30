63e4cc444141ac0b11188a95acc94c70a361b5d7
spopovic14 <s_popovic14@raf.edu.rs>
2017-05-30 14:40:13 +0200

    Removed getById method from User

--- app/Model/User.php
+++ app/Model/User.php
@@ -15,23 +15,6 @@ class User extends Model
 	protected $id;
 	protected $name;
 
-	const TABLE_NAME = 'user';
-
-
-	public static function getById($connection, $id)
-	{
-		$statement = $connection->prepare('SELECT id, name FROM ' . User::TABLE_NAME . ' where id = :id');
-		$statement->setFetchMode(Connection::FETCH_CLASS, static::class);
-		$statement->execute([':id' => $id]);
-		$user = $statement->fetch();
-		if($user == false)
-		{
-			$user = null;
-		}
-		return $user;
-	}
-
-
 	public function setId($id)
 	{
 		$this->id = $id;

5ca0df9c124d6fbdd3988c910b91b88bd133c571
spopovic14 <s_popovic14@raf.edu.rs>
2017-06-03 21:56:00 +0200

    Update Model.php

--- core/base/Model.php
+++ core/base/Model.php
@@ -5,6 +5,18 @@ namespace Core\Base;
 class Model
 {
 	
-	
+	public function toArray()
+	{
+		$ret = [];
+		$reflect = new \ReflectionClass($this);
+		$props = $reflect->getProperties(\ReflectionProperty::IS_PUBLIC);
+
+		foreach($props as $prop)
+		{
+			$name = $prop->getName();
+			$ret[$name] = $this->$name;
+		}
+		return $ret;
+	}
 
 }

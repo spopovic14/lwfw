bbe52b67bc6219a5349e84005b116969e7a16cf6
spopovic14 <s_popovic14@raf.edu.rs>
2017-06-03 22:03:00 +0200

    JsonResponse now casts Model objects to arrays.

--- core/http/JsonResponse.php
+++ core/http/JsonResponse.php
@@ -2,12 +2,18 @@
 
 namespace Core\Http;
 
+use Core\Base\Model;
+
 class JsonResponse extends Response
 {
 	
 	public function __construct($object, $content = '', $status = 200)
 	{
 		parent::__construct($content, $status, ['Content-Type' => 'application/json', 'Connection' => 'Close']);
+		if($object instanceof Model)
+		{
+			$object = $object->toArray();
+		}
 		$this->setContent(json_encode($object));
 	}
 
